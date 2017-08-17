62b1580aff82f7521965070c07cb8b46261e7f91
ulgoon <ulgoon89@gmail.com>
2017-08-17 19:24:32 +0900

    edit fizzbuzz.py elif -> else: if:

--- resources/fizzbuzz.py
+++ resources/fizzbuzz.py
@@ -3,9 +3,11 @@ limit = int(input("Type the number: "))
 for i in range(1, limit+1):
     if i % 15 == 0:
         print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-        print(i)
+    else: 
+        if i % 3 == 0:
+            print("fizz")
+        else:
+            if i % 5 == 0:
+                print("buzz")
+            else:
+                print(i)
