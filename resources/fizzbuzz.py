ebd23eaa3c63cc8c7741079235fdb18d0045a4e0
ulgoon <ulgoon89@gmail.com>
2017-08-17 19:26:44 +0900

    edit fizzbuzz.py to list comprehension(method1)

--- resources/fizzbuzz.py
+++ resources/fizzbuzz.py
@@ -1,13 +1,15 @@
 limit = int(input("Type the number: "))
 
-for i in range(1, limit+1):
-    if i % 15 == 0:
-        print("fizzbuzz")
-    else: 
-        if i % 3 == 0:
-            print("fizz")
-        else:
-            if i % 5 == 0:
-                print("buzz")
-            else:
-                print(i)
+#for i in range(1, limit+1):
+#    if i % 15 == 0:
+#        print("fizzbuzz")
+#    else: 
+#        if i % 3 == 0:
+#            print("fizz")
+#        else:
+#            if i % 5 == 0:
+#                print("buzz")
+#            else:
+#                print(i)
+
+print(["fizzbuzz" if i%15==0 else "fizz" if i%3==0 else "buzz" if i%5==0 else i for i in range(1,limit+1)])
