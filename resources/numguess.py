c809b196b8bc6d7ee1dc9afb4ad80565172555e2
ulgoon <ulgoon89@gmail.com>
2017-08-17 15:33:01 +0900

    add Iteration

--- resources/numguess.py
+++ resources/numguess.py
@@ -5,11 +5,12 @@ name = input("What is your name? ")
 
 answer = random.randint(1,100)
 print(answer)
+while True:
+    guess = int(input("Hi, " + name  + ". Type the number(1,100): "))
 
-guess = int(input("Hi, " + name  + ". Type the number(1,100): "))
-
-if guess == answer:
-    print("Correct!")
-else:
-    print("You are wrong!!!!!!!!!!")
+    if guess == answer:
+        print("Correct!")
+        break
+    else:
+        print("You are wrong!!!!!!!!!!")
 
